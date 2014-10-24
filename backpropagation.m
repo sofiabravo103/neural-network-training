fbf9d5adc4b08f8c9e1d377166a229c23202dee9
Sofia Bravo <sofiabravo103@gmail.com>
2014-10-24 18:21:46 -0430

    Added a max ammount of iterations

--- backpropagation.m
+++ backpropagation.m
@@ -4,8 +4,10 @@ function [new_weights_h, new_weights_o, error_vec] = backpropagation (data, weig
   new_weights_o = weights_2;
   error_vec = [];
   iter = 0;
+  max_iters = 10000;
 
-  while true
+  while iter < max_iters
+    printf('\r%d%s',(iter * 100)/ max_iters, '%');
     acumulated_error = 0;
     iter += 1;
     for i = 1:length(data)
@@ -22,8 +24,8 @@ function [new_weights_h, new_weights_o, error_vec] = backpropagation (data, weig
     error_vec = [error_vec; acumulated_error];
 
     if iter > 1 && (error_vec(iter - 1) > acumulated_error) ...
-      && (((error_vec(iter - 1) - acumulated_error) <= 0.00001));
-      disp(sprintf('Early exit by error convergence with %d iterations.', iter));
+      && (((error_vec(iter - 1) - acumulated_error) <= 0.0001));
+      disp(sprintf('\rEarly exit by error convergence with %d iterations.', iter));
       break
     endif
 
